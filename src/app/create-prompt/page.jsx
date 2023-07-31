069a037d8a313d8d3bd81e372e4fc5c833e3f20d
FahadAliSheikh <fahad.a.sheikh@gmail.com>
2023-07-31 17:07:15 +0500

    creat form added

---
+++ src/app/create-prompt/page.jsx
@@ -0,0 +1,23 @@
+"use client";
+import Form from "@/components/Form";
+import React, { useState } from "react";
+
+const CreatePrompt = () => {
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: "",
+    tag: "",
+  });
+  const createPrompt = async () => {};
+  return (
+    <Form
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
+    ></Form>
+  );
+};
+
+export default CreatePrompt;
